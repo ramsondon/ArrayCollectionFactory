a7d828f01a63054adf06abfdd664579866740c03
Matthias Schmid <ramsondon@gmail.com>
2014-08-08 20:05:38 +0200

    added public interface class

---
+++ src/Ramsondon/CollectionFactory.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * CollectionFactory.php
+ * copyright by Matthias Schmid <ramsondon@gmail.com>
+ * Date: 08/08/14
+ */
+
+namespace Ramsondon;
+
+use Ramsondon\TypedArray\TypedArrayFactory;
+
+/**
+ * Class CollectionFactory
+ *
+ * the public interface
+ *
+ * @package Ramsondon
+ */
+class CollectionFactory extends TypedArrayFactory
+{
+}
